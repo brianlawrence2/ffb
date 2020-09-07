4a7148ffbe6557bda26176db691f80f293349bd1
brianlawrence2 <brianlawrence2@gmail.com>
2020-09-07 03:19:45 +0000

    finished helper functions for pulling season data

--- helper_functions.R
+++ helper_functions.R
@@ -1,8 +1,25 @@
 library(tidyverse)
 library(rvest)
+library(glue)
+
+settings <- list(
+  teams = 12,
+  passing_yards = 25,
+  passing_td = 6,
+  int = -2,
+  rushing_yards = 10,
+  rushing_td = 6,
+  reception = 1,
+  receiving_yards = 10,
+  receiving_td = 6,
+  return_td = 6,
+  two_pt = 2,
+  fmbl = -2,
+  off_fmbl_return_td = 6
+)
 
 scrape_season_html <- function(season = 2019) {
-  url = str_c("https://www.pro-football-reference.com/years/", season, "/fantasy.htm#fantasy::none", sep = "")
+  url = glue("https://www.pro-football-reference.com/years/{season}/fantasy.htm#fantasy::none", sep = "")
   season_stats <- read_html(url)
   
   return(season_stats)
@@ -17,7 +34,17 @@ get_column_names <- function(tbl) {
     unite(`1`, `2`, col = "col_names", sep = "_") %>%
     select(col_names)
   
-  return(cols$col_names)
+  cols <- cols$col_names %>%
+    str_extract("[A-Za-z]+\\_?[A-Za-z0-9]*")
+  
+  return(cols)
+}
+
+clean_names <- function(names) {
+  return(
+    names %>%
+      str_extract("[A-Za-z\'\\.\\-]+\\s[A-Za-z\\.\\-]+")
+  )
 }
 
 format_season <- function(season_html) {
@@ -27,18 +54,57 @@ format_season <- function(season_html) {
   
   season_tbl <- season_l[[1]]
   colnames(season_tbl) <- season_tbl %>% get_column_names
-  season_tbl <- season_tbl %>% slice(3:n())
+  season_tbl <- season_tbl %>% 
+    slice(3:n()) %>%
+    filter(Player != "Player")
+  
+  season_tbl$Player <- season_tbl$Player %>% clean_names
+  
   
   return(season_tbl)
 }
 
-scrape_season <- function(season = 2019) {
-  return(
-    scrape_season_html(season) %>%
-      format_season
-  )
+calculate_league_scoring <- function(season, settings) {
+  season$league_score <- season$Passing_Yds / settings$passing_yards +
+        season$Passing_TD * settings$passing_td +
+        season$Passing_Int * settings$int +
+        season$Rushing_Yds / settings$rushing_yards +
+        season$Rushing_TD * settings$rushing_td +
+        season$Receiving_Rec * settings$reception +
+        season$Receiving_Yds / settings$receiving_yards +
+        season$Receiving_TD * settings$receiving_td +
+        season$Fumbles_FL * settings$fmbl +
+        season$Scoring_2PM * settings$two_pt +
+        season$Scoring_2PP * settings$two_pt      
+  
+  return(season)
 }
 
+calculate_league_rank <- function(season) {
+  season <- season %>%
+    mutate(
+      league_rank = row_number(desc(league_score))
+    )
+  
+  season <- season %>%
+    group_by(FantPos) %>%
+    mutate(
+      league_pos_rank = row_number(desc(league_score))
+    )
+  
+  return(season)
+}
 
+scrape_season <- function(season = 2019) {
+  season_data <- scrape_season_html(season) %>%
+    format_season %>%
+    type_convert %>%
+    mutate(
+      across(everything(), ~replace_na(.x, 0))
+    ) %>%
+    calculate_league_scoring(settings) %>%
+    calculate_league_rank
+  
+  return(season_data)
+}
 
-scrape_season(2019)
